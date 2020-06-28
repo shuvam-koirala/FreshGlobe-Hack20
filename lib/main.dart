1e2bf86e6af686babdfcd37d510070db10fb6806
curious coder <suvamkoirala08@gmail.com>
2020-06-29 01:28:31 +0545

    suvam

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@ import 'package:freshglobe/profile/profile.dart';
 import 'package:freshglobe/register.dart';
 import 'login.dart';
 import 'particaleffect.dart';
+import 'profile/profile.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/',
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/profile',
     routes: {
       '/': (context) => HomeScreen(),
       '/login': (context) => CircularParticleLogin(),
--- lib/profile/profile.dart
+++ lib/profile/profile.dart
@@ -11,40 +11,42 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              fit: BoxFit.cover,
-              image: AssetImage("images/mandala-background.jpg")),
-        ),
-        child: Align(
-          alignment: Alignment.center,
-          child: Container(
-            height: MediaQuery.of(context).size.height * 0.9,
-            width: MediaQuery.of(context).size.width * 0.9,
-            decoration: BoxDecoration(
-                color: Colors.lightGreen[400],
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(10)),
-            child: Row(
-              children: [
-                Profile1(),
-                verticalDivider(),
-                Column(
-                  children: [
-                    Profile2(),
-                    Divider(
-                      color: Colors.grey,
-                    ),
-                    Profile3(),
-                  ],
-                ),
-              ],
-            ),
+    return Scaffold(
+      body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("images/mandala-background.jpg")),
           ),
-        ));
+          child: Align(
+            alignment: Alignment.center,
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.9,
+              width: MediaQuery.of(context).size.width * 0.9,
+              decoration: BoxDecoration(
+                  color: Colors.lightGreen[400],
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(10)),
+              child: Row(
+                children: [
+                  Profile1(),
+                  verticalDivider(),
+                  Column(
+                    children: [
+                      Profile2(),
+                      Divider(
+                        color: Colors.grey,
+                      ),
+                      Profile3(),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
   }
 }
 
