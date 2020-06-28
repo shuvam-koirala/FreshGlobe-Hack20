83cd78b1c39688198faf84fd59fb6305a5b88293
Sheetal Guragain <sheetalguragain@gmail.com>
2020-06-28 17:35:27 +0545

    *Sheetal *

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:freshglobe/register.dart';
 import 'login.dart';
 import 'particaleffect.dart';
 
 void main() {
   runApp(MaterialApp(
-    initialRoute: '/login',
+    initialRoute: '/register',
     routes: {
       '/': (context) => HomeScreen(),
       '/login': (context) => CircularParticleLogin(),
+      '/register': (context) => CircularParticleRegister(),
     },
   ));
 }
